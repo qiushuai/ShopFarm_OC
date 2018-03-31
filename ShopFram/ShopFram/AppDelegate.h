09d8e1251f0b4a0771341172fc6fcf0da23ac7f0
王三坤 <wangsankun@192.168.1.120>
2018-03-31 11:19:05 +0800

    创建项目

---
+++ ShopFram/ShopFram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopFram
+//
+//  Created by 王三坤 on 2018/3/31.
+//  Copyright © 2018年 王三坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShopFram/ShopFram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShopFram
+//
+//  Created by 王三坤 on 2018/3/31.
+//  Copyright © 2018年 王三坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
